49266f9721d989241506027a5c973655f2f0b55e
Aliaksei Korbut <akorbut@edu.cdv.pl>
2019-03-25 15:58:18 +0100

    python

--- programowanie_strukturalna/1_podstawy.py
+++ programowanie_strukturalna/1_podstawy.py
@@ -1,1 +1,105 @@
 print('CDV')
+print('2')
+
+
+
+#potęga
+potega = 2 ** 10
+print(potega)
+
+
+
+
+
+
+
+
+
+
+
+
+#pobiranie danych z klawiatury
+imie= input()
+print("twoje imię padane z klawiatury: " + imie)
+
+
+
+
+nazwisko = input()
+print("Twoje imię: " + imie + ", Twoje nazwisko:" + nazwisko)
+
+dlugosc = len(nazwisko)
+print(type(dlugosc))
+print('Dlugosc nazwiska',dlugosc)
+
+dlugosc=str(dlugosc)
+print(type(dlugosc))
+print('Dlugosc nazwiska'+dlugosc)
+
+print('')
+
+print('podaj swoj wiek')
+
+wiek= input()
+print(type(wiek))
+print("twoje wiek padane z klawiatury: " + wiek + ' lat')
+
+
+#uzytkownik padaje swoj wiek, wyswietl dane w formacie : Twoj wiek: *zmiena* lat
+
+
+x="5"
+print(type(x)) #str
+x=int(x)
+print(type(x)) #int
+
+text = "Jan" * 3
+print(text)
+
+
+y=6
+print(type(y)) #int
+y=y/2
+print(type(y)) #float
+print(y) #3.0
+wiek = 21
+print("Twoj wiek:" , wiek)
+wiek = str(wiek)
+print("Twoj wiek:" + wiek)
+
+nazwisko = 'Kankowski'
+print(nazwiko[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('')
+
+
